c9fd6d3cdd1cf03165edcaf43484b688c1af3424
second1996 <vital10012@gmail.com>
2019-10-04 11:59:22 +0300

    Sticky header

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -39,4 +39,58 @@ jQuery(function() {
 		$mNavOverlay.removeClass('is-active');
 	});
 
+	// Hide Header when scrolling down
+	$(window).scroll(function(){
+		if($(window).scrollTop() >= 150){
+			$(".header").addClass('header-light');
+		}
+		else {
+			$(".header").removeClass('header-light');
+		}
+	});
+	$(document).ready(function(){
+		if($(window).scrollTop() >= 150){
+			$(".header").addClass('header-light');
+		}
+		else {
+			$(".header").removeClass('header-light');
+		}
+	});
+
+	$('document').ready(function() {
+
+		var didScroll;
+		var lastScrollTop = 0;
+		var delta = 5;
+		var navbarHeight = $('.header').outerHeight();
+
+		$(window).scroll(function(event){
+			didScroll = true;
+		});
+
+		setInterval(function() {
+			if (didScroll) {
+				hasScrolled();
+				didScroll = false;
+			}
+		}, 250);
+
+		function hasScrolled() {
+			var st = $(this).scrollTop();
+
+			if (Math.abs(lastScrollTop - st) <= delta)
+				return;
+			
+			if (st > lastScrollTop && st > navbarHeight){
+				$('.header').removeClass('header-hide').addClass('header-show');
+			} else {
+				if(st + $(window).height() < $(document).height()) {
+					$('.header').removeClass('header-show').addClass('header-hide');
+				}
+			}
+			
+			lastScrollTop = st;
+		}
+	});
+
 });
